2d355ac227ee7a113475a7b9b12f694cd3e1ac63
clarkk <clark@electrobeat.dk>
2021-12-15 19:10:08 +0100

    fixed apply timezone

--- time.js
+++ time.js
@@ -1,46 +1,31 @@
 'use strict';
 
 (function(root){
-	function get(date){
-		let hour = date.getUTCHours(), min = date.getUTCMinutes(), sec = date.getUTCSeconds();
-		return {
-			year : date.getUTCFullYear(),
-			month : date.getUTCMonth() + 1,
-			day : date.getUTCDate(),
-			hour : hour,
-			min : min,
-			sec : sec,
-			weekday : date.getUTCDay(),
-			time : zerofill(hour, 2)+':'+zerofill(min, 2),
-			timesec : zerofill(hour, 2)+':'+zerofill(min, 2)+':'+zerofill(sec, 2)
-		};
-	}
-	
-	function zerofill(num, width){
-		let zeros = Math.max(0, width - Math.floor(num).toString().length);
-		return Math.pow(10, zeros).toString().substr(1)+num;
-	}
-	
 	root.Time = {
 		init(lang){
 			
 		},
-		time_date(time, mode='full', apply_timezone=false){
-			let date = new Date();
+		date(time, mode='full', apply_timezone=false){
+			let date;
+			
 			if(time != 'now'){
 				if(!parseInt(time)){
 					return '';
 				}
 				
+				date = new Date(time * 1000);
 				if(apply_timezone){
-					time -= new Date(time * 1000).getTimezoneOffset() * 60;
+					date.setTime((time - timezone_offset(date)) * 1000);
 				}
-				
-				date.setTime(time * 1000);
+			}
+			else{
+				date = new Date();
+				if(apply_timezone){
+					date.setTime((date.getTime() / 1000 - timezone_offset(date)) * 1000);
+				}
 			}
 			
 			let d = get(date);
-			
 			switch(mode){
 				case 'full':
 					d.datestamp = zerofill(d.day, 2)+'-'+zerofill(d.month, 2)+'-'+d.year;
@@ -59,4 +44,28 @@
 			return d;
 		}
 	};
-}(this);
+	
+	function get(date){
+		let hour = date.getUTCHours(), min = date.getUTCMinutes(), sec = date.getUTCSeconds();
+		return {
+			year : date.getUTCFullYear(),
+			month : date.getUTCMonth() + 1,
+			day : date.getUTCDate(),
+			hour : hour,
+			min : min,
+			sec : sec,
+			weekday : date.getUTCDay(),
+			time : zerofill(hour, 2)+':'+zerofill(min, 2),
+			timesec : zerofill(hour, 2)+':'+zerofill(min, 2)+':'+zerofill(sec, 2)
+		};
+	}
+	
+	function timezone_offset(date){
+		return date.getTimezoneOffset() * 60;
+	}
+	
+	function zerofill(num, width){
+		let zeros = Math.max(0, width - Math.floor(num).toString().length);
+		return Math.pow(10, zeros).toString().substr(1)+num;
+	}
+})(this);
